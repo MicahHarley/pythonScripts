e5c4ffd2a14550ba80513b21411568389969ae11
eop <mharley@healthwise.org>
2020-05-08 13:13:12 -0600

    Sharing

--- http_stream.py
+++ http_stream.py
@@ -5,19 +5,23 @@ import cv2
 url = 'http://192.168.2.4:8000/video_feed'
 
 vcap = cv2.VideoCapture(url)
+vcap.set(cv2.CAP_PROP_BUFFERSIZE,1) 
+FPS = 1/30
+FPS_MS = int(FPS * 1000)
+
 time.sleep(2.0)
 
 while True:
     # grab the frame from the threaded video stream and resize it
     # to have a maximum width of 400 pixels
     ret, frame = vcap.read()
-    # frame = vs.read()
-    frame = imutils.resize(frame, width=400)
-
+    #frame = imutils.resize(frame, width=400)
+    
+    if ret:
         # show the output frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-
+        cv2.imshow("Frame", frame)
+    
+    key = cv2.waitKey(FPS_MS) & 0xFF
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
